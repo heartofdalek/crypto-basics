bf136c01fc9ef406ed0b601f1f00a359147d3cf4
heartofdalek <breeze@2sun.su>
2025-09-16 17:06:56 +0300

    show methods in help

--- run.py
+++ run.py
@@ -3,11 +3,14 @@
 from argparse import ArgumentParser as OptionParser
 from enc.sf import EncryptStrategy
 
+sf = EncryptStrategy()
+allowed_strategies = sf.allowed_strategies.keys()
+
 parser = OptionParser(description='Make symmetric encryption with some basic algo\'s')
 
-parser.add_argument("-k", "--key", dest="key", default="8", help="encryption key: int from 0 to 16")
+parser.add_argument("-k", "--key", dest="key", default="8", help="encryption key")
 parser.add_argument("-m", "--map", dest="mapping", default="mapping_orig.txt", help="char mapping file")
-parser.add_argument("-s", "--strategy", dest="strategy", default="caesar_simple", help="encryption strategy method: caesar_simple, caesar_mapped")
+parser.add_argument("-s", "--strategy", dest="strategy", default="caesar_simple", help="encryption strategy method: {}".format(", ".join(allowed_strategies)))
 parser.add_argument("-t", "--type", dest="type", default="encode", help="encryption direction: encode, decode")
 parser.add_argument("-d", "--debug", dest="is_debug", action='store_true', default=False, help="enable debug if specified")
 parser.add_argument("payload", type=str, nargs='?', default="", help="text to encode/decode")
@@ -18,7 +21,7 @@ payload = options.payload if len(options.payload) else ""
 
 try:
     
-    strategy = EncryptStrategy().create(options.strategy)
+    strategy = sf.create(options.strategy)
     strategy.load(options)
     
     result = strategy.call(options, payload)
