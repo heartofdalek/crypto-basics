d4b30c8225fa9ee74e3277eafaba4b557f406a00
heartofdalek <breeze@2sun.su>
2025-09-21 14:39:25 +0300

    change labels

--- run.sh
+++ run.sh
@@ -8,25 +8,25 @@ PAYLOAD='HELLO, MY FRIEND! STAY AWHILE AND LISTEN! SOOOME LOOOONG STRIIING CHAAA
 echo ""
 
 print_test_data() {
-    echo "Метод| ${1}"
-    echo "Ключ| ${2}"
-    echo "Текст| ${3}"
-    echo "Шифр| ${4}"
-    echo "Дешифр| ${5}"
+    echo "Method| ${1}"
+    echo "Key| ${2}"
+    echo "Text| ${3}"
+    echo "Encrypted| ${4}"
+    echo "Decrypted| ${5}"
     echo ""
 }
 
 ENCODED_PAYLOAD=$(./cryptobase.py -k ${CAESAR_KEY} -s caesar_mapped -t encode "${PAYLOAD}")
 DECODED_PAYLOAD=$(./cryptobase.py -k ${CAESAR_KEY} -s caesar_mapped -t decode "${ENCODED_PAYLOAD}")
 
-print_test_data "Цезарь, с подстановочным алфавитом" $CAESAR_KEY "${PAYLOAD}" "${ENCODED_PAYLOAD}" "${DECODED_PAYLOAD}" | column -tL  -s "|"
+print_test_data "Caesar with substitute alphabet" $CAESAR_KEY "${PAYLOAD}" "${ENCODED_PAYLOAD}" "${DECODED_PAYLOAD}" | column -tL  -s "|"
 
 ENCODED_PAYLOAD=$(./cryptobase.py -k ${CAESAR_KEY} -s caesar_simple -t encode "${PAYLOAD}")
 DECODED_PAYLOAD=$(./cryptobase.py -k ${CAESAR_KEY} -s caesar_simple -t decode "${ENCODED_PAYLOAD}")
 
-print_test_data "Цезарь, с базовым алфавитом" $CAESAR_KEY "${PAYLOAD}" "${ENCODED_PAYLOAD}" "${DECODED_PAYLOAD}" | column -tL  -s "|"
+print_test_data "Caesar basic" $CAESAR_KEY "${PAYLOAD}" "${ENCODED_PAYLOAD}" "${DECODED_PAYLOAD}" | column -tL  -s "|"
 
 ENCODED_PAYLOAD=$(./cryptobase.py -k ${SUBST_KEY} -s subst_cfb -t encode "${PAYLOAD}")
 DECODED_PAYLOAD=$(./cryptobase.py -k ${SUBST_KEY} -s subst_cfb -t decode "${ENCODED_PAYLOAD}")
 
-print_test_data "Замена по подстановочному алфавиту + CFB" $SUBST_KEY "${PAYLOAD}" "${ENCODED_PAYLOAD}" "${DECODED_PAYLOAD}" | column -tL  -s "|"
+print_test_data "Modified Vigenere + CFB" $SUBST_KEY "${PAYLOAD}" "${ENCODED_PAYLOAD}" "${DECODED_PAYLOAD}" | column -tL  -s "|"
